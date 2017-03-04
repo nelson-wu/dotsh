3df9bc240340c27950cd9ba268270acc01c58be3
nelson-wu <wu_nelson@outlook.com>
2017-03-04 09:49:55 +0900

    more failsafe alarm

--- mpdalarm.sh
+++ mpdalarm.sh
@@ -2,23 +2,31 @@
 # USAGE:
 # mpdalarm.sh [ alarm_time (HH:MM) ] [ album_name ]
 # mpdalarm.sh [ alarm_time (HH:MM) ]
-if [ $(date -d $1 |& grep -c invalid) != "0" ]
+if [ $(date -d $1 |& grep -c invalid) != "0" ] || [ $# -eq 0 ]
 then
     echo "Invalid time format."
     echo "mpdalarm.sh [ alarm_time (HH:MM) ] [ album_name ]"
     echo "mpdalarm.sh [ alarm_time (HH:MM) ]"
     exit 1
 fi
-if [ $(mpc playlist | wc -l) == 0 ] 
+
+if  [ $# -eq 2 ] && [  $( mpc search album $2 | wc -l ) -eq 0 ]
 then
-    if [ $(mpc search album $2 | wc -l) == 0 ]
-    then
-        echo "Playlist is empty and specified album not found."
-        exit 1
-    fi
+    echo "Specified album not found."
+    exit 1
 fi
+
 while [ $(date +%R) != $1 ]; do
     sleep 10
 done
+
+if [ $# -eq 2 ]
+then
+    mpc search album $2 | mpc add
+else
+    mpc listall | shuf -n 5 | mpc add
+fi
+
 mpc repeat on
+mpc single off
 mpc play
